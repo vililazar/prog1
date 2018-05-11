1677716d167600f5f61c0be2f7855cbd2660bba6
vililazar <37209781+vililazar@users.noreply.github.com>
2018-05-11 14:30:17 +0200

    Update beadandó11.py

--- beadandó11.py
+++ beadandó11.py
@@ -1,16 +1,20 @@
-#import timeit
-#start = timeit.default_timer()
+# import timeit
+# start = timeit.default_timer()
 
-def beadando_11():
-    for i in range(999,99,-1):
-        for j in range(999,99,-1):
-            product = i * j
-            if str(product) == str(product)[::-1]:
-                loop1 = i
-                loop2 = j
-                p = product
-                return '{0} * {1} = {2}'.format(loop1, loop2, p)
-print(beadando_11())
+def feladat():
+    max = 0
+    szam1 = 0
+    szam2 = 0
+    for i in range(100,1000):
+        for j in range(100,1000):
+            x = i*j
+            if str(x)==str(x)[::-1]:
+                if x>max:
+                    max = x
+                    szam1 = i
+                    szam2 = j
+    return '{0} * {1} = {2}'.format(szam1,szam2,max)
+print(feladat())
 
-#stop = timeit.default_timer()
-#print(stop - start)
+# stop = timeit.default_timer()
+# print(stop - start)
